c6ad8364ee9782e42bfa2b3121d55f423fae6455
nakaso2001 <200250gz@yse-c.net>
2021-10-22 11:18:54 +0900

    データベースエラー

--- syukka.php
+++ syukka.php
@@ -13,13 +13,13 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (session_starts() == PHP_SESSION_NONE/* ①の処理を行う */) {
-	//②セッションを開始するc
+if (session_status() == PHP_SESSION_NONE/* ①の処理を行う */) {
+	//②セッションを開始する
 	session_start();
 }
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (empty($_SESSION['login']) || $_SESSION['login'] == false/* ③の処理を書く */){
+if (!$_SESSION['login']/* ③の処理を書く */){
 	//④SESSIONの「error2」に「ログインしてください」と設定する。
 	$_SESSION['error2'] == "ログインしてください";
 	//⑤ログイン画面へ遷移する。
@@ -33,7 +33,7 @@ $db_port = '3306';
 $db_user = 'zaiko2021_yse';
 $db_password = '2021zaiko';
 
-$dsn = "mysql:dbname{$db_name};host={$db_host};charset=utf-8;port={$db_port}";
+$dsn = "mysql:dbname={$db_name};host={$db_host};charset=utf-8;port={$db_port}";
 try {
 	$pdo = new PDO($dsn, $db_user, $db_password);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -41,7 +41,7 @@ try {
 } catch (PDOException $e) {
 	echo "接続失敗: " . $e->getMessage();
 	exit;
-
+}
 
 //⑦データベースで使用する文字コードを「UTF8」にする
 
@@ -59,8 +59,9 @@ function getId($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-
+	$sql="SELECT*FROM books WHERE id={$id}";
 	//⑫実行した結果から1レコード取得し、returnで値を返す。
+	return $con->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -72,74 +73,75 @@ function getId($id,$con){
 <link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
 <body>
-<!-- ヘッダ -->
-<div id="header">
-	<h1>出荷</h1>
-</div>
-
-<!-- メニュー -->
-<div id="menu">
-	<nav>
-		<ul>
-			<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
-		</ul>
-	</nav>
-</div>
-
-<form action="syukka_kakunin.php" method="post">
-	<div id="pagebody">
-		<!-- エラーメッセージ -->
-		<div id="error">
-		<?php
-		 // ⑬SESSIONの「error」にメッセージが設定されているかを判定する。定されていた場合はif文の中に入る。
-		if(isset($_SESSION['error'])){
-			//⑭SESSIONの「error」の中身を表示する。
-			$_SESSION['error'] = "";
-			echo $_SESSION['error'];
-		}
-		?>
-		</div>
-		<div id="center">
-			<table>
-				<thead>
-					<tr>
-						<th id="id">ID</th>
-						<th id="book_name">書籍名</th>
-						<th id="author">著者名</th>
-						<th id="salesDate">発売日</th>
-						<th id="itemPrice">金額(円)</th>
-						<th id="stock">在庫数</th>
-						<th id="in">出荷数</th>
-					</tr>
-				</thead>
-				<?php 
-				/*
-				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
-				 */
-				foreach($_POST['books'] as $key => $booksbox/* ⑮の処理を書く */){
-					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-				?>
-				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
-				<tr>
-					<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
-					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
-				</tr>
-				<?php
-				}
-				?>
-			</table>
-			<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
-		</div>
+	<!-- ヘッダ -->
+	<div id="header">
+		<h1>出荷</h1>
 	</div>
-</form>
-<!-- フッター -->
-<div id="footer">
-	<footer>株式会社アクロイト</footer>
-</div>
+
+	<!-- メニュー -->
+	<div id="menu">
+		<nav>
+			<ul>
+				<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
+			</ul>
+		</nav>
+	</div>
+
+	<form action="syukka_kakunin.php" method="post">
+		<div id="pagebody">
+			<!-- エラーメッセージ -->
+			<div id="error">
+			<?php
+			// ⑬SESSIONの「error」にメッセージが設定されているかを判定する。定されていた場合はif文の中に入る。
+			if(isset($_SESSION['error'])){
+				//⑭SESSIONの「error」の中身を表示する。
+				$_SESSION['error'] = "";
+				echo '<p>'.$_SESSION['error'].'<p>';
+			}
+			?>
+			</div>
+			<div id="center">
+				<table>
+					<thead>
+						<tr>
+							<th id="id">ID</th>
+							<th id="book_name">書籍名</th>
+							<th id="author">著者名</th>
+							<th id="salesDate">発売日</th>
+							<th id="itemPrice">金額(円)</th>
+							<th id="stock">在庫数</th>
+							<th id="in">出荷数</th>
+						</tr>
+					</thead>
+					<?php 
+					/*
+					* ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
+					*/
+						foreach($_POST['books'] as $book/* ⑮の処理を書く */){
+						// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
+						$books=getId($book,$pdo);
+					?>
+					<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */$books["id"];?>" name="books[]">
+					<tr>
+						<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */$books["id"];?></td>
+						<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */$books["title"];?></td>
+						<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */$books["author"];?></td>
+						<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */$books["salesDate"];?></td>
+						<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */$books["price"];?></td>
+						<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */$books["stock"];?></td>
+						<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
+					</tr>
+					<?php
+					 }
+					?>
+				</table>
+				<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
+			</div>
+		</div>
+	</form>
+	<!-- フッター -->
+	<div id="footer">
+		<footer>株式会社アクロイト</footer>
+	</div>
 </body>
 </html>
