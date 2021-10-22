e2f860a4d25393d07c27a4671e4a51aca90783c9
sss-yyy <200081on@yse-c.net>
2021-10-22 09:18:35 +0900

    master

--- syukka.php
+++ syukka.php
@@ -116,7 +116,7 @@ function getId($id,$con){
 				/*
 				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 				 */
-				foreach(/* ⑮の処理を書く */){
+				foreach($_POST['books'] as $key => $booksbox/* ⑮の処理を書く */){
 					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
 				?>
 				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
